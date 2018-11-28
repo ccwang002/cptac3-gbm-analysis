a0043b095854afe7f12634c4f113815905b50ab9
Liang-Bo Wang <liang-bo.wang@wustl.edu>
2018-11-28 11:06:07 -0600

    Load both annotated and raw mutation calls

--- 201811_proteomic_qc/scripts/make_db.py
+++ 201811_proteomic_qc/scripts/make_db.py
@@ -1,9 +1,7 @@
 import argparse
-from collections import namedtuple
-from typing import NamedTuple
 import logging
 from pathlib import Path
-import re
+from typing import NamedTuple
 from sqlalchemy import (
     create_engine, event,
     MetaData, Table, Column, Integer, Text, Index,
@@ -11,12 +9,13 @@ from sqlalchemy import (
 )
 from sqlalchemy.engine import Engine
 from cyvcf2 import VCF
+from maf_utils import GDCMAF
 
 logger = logging.getLogger(__name__)
 BATCH_SIZE = 5000
 
-GROUP_GDC_RAW_CALLS_SQL = """\
-CREATE TABLE gdc_raw AS
+GROUP_GDC_ANNOTATED_CALLS_SQL = """\
+CREATE TABLE gdc AS
 SELECT
     sample, chromosome, start, end, ref_allele, alt_allele,
     avg(t_depth) AS t_depth,
@@ -27,9 +26,13 @@ SELECT
     avg(n_alt_count) AS n_alt_count,
     avg(t_alt_count) / avg(t_depth) AS t_allele_freq,
     avg(n_alt_count) / avg(n_depth) AS n_allele_freq,
-    group_concat(caller, '|') AS callers
-FROM gdc_raw_per_caller
-WHERE filter IS NULL
+    group_concat(caller, '|') AS callers,
+    group_concat("filter", '|') AS "filter",
+    variant_type, variant_classification,
+    symbol, gene, transcript_id, tsl, biotype, consequence, canonical,
+    hgvsc, hgvsp, hgvsp_short,
+    all_effects
+FROM gdc_per_caller
 GROUP BY sample, chromosome, start, end, ref_allele, alt_allele
 """
 
@@ -51,30 +54,6 @@ class RawMutationCall(NamedTuple):
     n_alt_count: int
 
 
-class AnnotatedMutationCall(NamedTuple):
-    sample: str
-    chromosome: str
-    start: int
-    end: int
-    ref_allele: str
-    alt_allele: str
-    filter: str
-    caller: str
-    t_depth: int
-    t_ref_count: int
-    t_alt_count: int
-    n_depth: int
-    n_ref_count: int
-    n_alt_count: int
-    symbol: str
-    transcript: str
-    tls: int
-    biotype: str
-    consequence: str
-    hgvs_c: str
-    hgvs_p: str
-
-
 def cal_read_counts(mut, caller):
     """Extract the read count from the VCF."""
     if caller == 'muse' or caller == 'mutect2':
@@ -89,62 +68,34 @@ def cal_read_counts(mut, caller):
         n_depth, t_depth = mut.format('DP')[:, 0]
         n_ref_count, t_ref_count = mut.format('DP4')[:, :2].sum(axis=1)
         n_alt_count, t_alt_count = mut.format('DP4')[:, 2:].sum(axis=1)
-    return t_depth, t_ref_count, t_alt_count, n_depth, n_ref_count, n_alt_count
+    return [int(x) for x in [t_depth, t_ref_count, t_alt_count, n_depth, n_ref_count, n_alt_count]]
 
 
-def read_raw_vcf(vcf_p):
-    """Read raw VCF"""
-    sample, caller, *_ = Path(vcf_p).name.split('.')
-    for mut in VCF(str(vcf_p)):
-        # Convert chromosome
-        if mut.CHROM == 'chrM':
-            chrom = 'MT'
-        else:
-            chrom = mut.CHROM[3:]
-
-        read_counts = list(map(int, cal_read_counts(mut, caller)))
-
-        yield RawMutationCall(
-            sample,
-            chrom, mut.start, mut.end,
-            mut.REF, mut.ALT[0],
-            mut.FILTER,
-            caller,
-            *read_counts
-        )
-
-
-
-def create_cols(namedtuple_cls):
-    """Create SQLAlchemy columns based on a named tuple class."""
-    columns = []
-    for field, f_type in namedtuple_cls._field_types.items():
-        if f_type is int:
-            col = Column(field, Integer())
-        else:
-            col = Column(field, Text())
-        columns.append(col)
-    return columns
-
-
-def define_db_schema(metadata):
-    Table(
-        'gdc_raw_per_caller', metadata,
-        *create_cols(RawMutationCall)
-    )
-
-
-def read_vcfs(vcf_pths):
+def read_all_raw_vcfs(vcf_pths):
+    """Read raw GDC VCF"""
     for vcf_p in vcf_pths:
         logger.info(f'... reading {vcf_p}')
-        yield from read_raw_vcf(vcf_p)
+        sample, caller, *_ = Path(vcf_p).name.split('.')
+        for mut in VCF(str(vcf_p)):
+            # Convert chromosome
+            if mut.CHROM == 'chrM':
+                chrom = 'MT'
+            else:
+                chrom = mut.CHROM[3:]
+            # Get read counts
+            read_counts = cal_read_counts(mut, caller)
+            yield RawMutationCall(
+                sample, chrom, mut.start, mut.end,
+                mut.REF, mut.ALT[0], mut.FILTER, caller,
+                *read_counts
+            )
 
 
 def load_vcfs_to_db(conn, metadata, vcf_pths):
     ins = metadata.tables['gdc_raw_per_caller'].insert()
     # Insert mutation calls by batches
     ins_batch = []
-    for i, mut in enumerate(read_vcfs(vcf_pths), 1):
+    for i, mut in enumerate(read_all_raw_vcfs(vcf_pths), 1):
         # Add new record into batch
         ins_batch.append(mut._asdict())
 
@@ -164,20 +115,119 @@ def load_vcfs_to_db(conn, metadata, vcf_pths):
     logger.info(f'Loaded total {i:,d} mutation calls')
 
 
-def main(db_pth, gdc_raw_vcfs):
-    logger.info(f'Load mutation calls to SQLite at {db_pth}')
-    logger.info(f'... loading {len(gdc_raw_vcfs)} VCFs')
+class AnnotatedMutationCall(NamedTuple):
+    sample: str
+    chromosome: str
+    start: int
+    end: int
+    ref_allele: str
+    alt_allele: str
+    filter: str
+    caller: str
+    t_depth: int
+    t_ref_count: int
+    t_alt_count: int
+    n_depth: int
+    n_ref_count: int
+    n_alt_count: int
+    variant_type: str
+    variant_classification: str
+    symbol: str
+    gene: str
+    transcript_id: str
+    tsl: str
+    biotype: str
+    consequence: str
+    canonical: str
+    hgvsc: str
+    hgvsp: str
+    hgvsp_short: str
+    all_effects: str
+
+
+def read_all_mafs(maf_pths):
+    def to_int(x):
+        if x == '':
+            return 0
+        else:
+            return int(x)
+
+    for maf_p in maf_pths:
+        logger.info(f'... reading {maf_p}')
+        sample, caller, *_ = Path(maf_p).name.split('.')
+        maf = GDCMAF(maf_p, caller=caller)
+        for m in maf:
+            yield AnnotatedMutationCall(
+                sample=sample, chromosome=m.chromosome, start=int(m.start_position), end=int(m.end_position),
+                ref_allele=m.reference_allele, alt_allele=m.tumor_seq_allele2,
+                filter=m.filter, caller=m.caller,
+                t_depth=to_int(m.t_depth), t_ref_count=to_int(m.t_ref_count), t_alt_count=to_int(m.t_alt_count),
+                n_depth=to_int(m.n_depth), n_ref_count=to_int(m.n_ref_count), n_alt_count=to_int(m.n_alt_count),
+                variant_type=m.variant_type, variant_classification=m.variant_classification,
+                symbol=m.symbol, gene=m.gene, transcript_id=m.transcript_id, tsl=m.tsl,
+                biotype=m.biotype, consequence=m.consequence, canonical=m.canonical,
+                hgvsc=m.hgvsc, hgvsp=m.hgvsp, hgvsp_short=m.hgvsp_short,
+                all_effects=m.all_effects
+            )
+
+
+def load_mafs_to_db(conn, metadata, maf_pths):
+    ins = metadata.tables['gdc_per_caller'].insert()
+    # Insert mutation calls by batches
+    ins_batch = []
+    for i, mut in enumerate(read_all_mafs(maf_pths), 1):
+        # Add new record into batch
+        ins_batch.append(mut._asdict())
+
+        # Commit the batch when a batch is full
+        if len(ins_batch) >= BATCH_SIZE:
+            with conn.begin():
+                conn.execute(ins, ins_batch)
+            ins_batch = []
+
+        if i % 50000 == 0:
+            logger.info(f'... processed {i:,d} mutation calls')
+
+    # Load the last batch
+    if ins_batch:
+        with conn.begin():
+            conn.execute(ins, ins_batch)
+    logger.info(f'Loaded total {i:,d} mutation calls')
+
+
+
+def create_cols(namedtuple_cls):
+    """Create SQLAlchemy columns based on a named tuple class."""
+    columns = []
+    for field, f_type in namedtuple_cls._field_types.items():
+        if f_type is int:
+            col = Column(field, Integer())
+        else:
+            col = Column(field, Text())
+        columns.append(col)
+    return columns
+
+
+def define_db_schema(metadata):
+    """Define the SQLite database schema."""
+    Table('gdc_raw_per_caller', metadata, *create_cols(RawMutationCall))
+    Table('gdc_per_caller', metadata, *create_cols(AnnotatedMutationCall))
+
+
+def main(db_pth, gdc_raw_vcfs, gdc_mafs):
+    logger.info(f'Build SQLite database at {db_pth}')
     metadata = MetaData()
     db_engine = create_engine('sqlite:///' + db_pth)
-
     define_db_schema(metadata)
     metadata.create_all(db_engine)
-
     conn = db_engine.connect()
-    load_vcfs_to_db(conn, metadata, gdc_raw_vcfs)
 
-    logger.info(f'Group all GDC raw calls')
-    conn.execute(GROUP_GDC_RAW_CALLS_SQL)
+    logger.info(f'Load GDC raw mutation calls from {len(gdc_raw_vcfs)} VCFs')
+    load_vcfs_to_db(conn, metadata, gdc_raw_vcfs)
+    logger.info(f'Load GDC annotated mutation calls from {len(gdc_mafs)} MAFs')
+    load_mafs_to_db(conn, metadata, gdc_mafs)
+    logger.info(f'Group calls from the multiple callers')
+    conn.execute(GROUP_GDC_ANNOTATED_CALLS_SQL)
 
     logger.info('Complete')
 
@@ -202,51 +252,6 @@ def setup_cli():
     console.setFormatter(log_formatter)
 
 
-def pick_effect(mut, all_effects):
-    """Pick the most representative effect of a mutation."""
-    pass
-
-
-def read_annotated_vcf(vcf_p):
-    """Read annotated VCF"""
-    sample, caller, *_ = Path(vcf_p).name.split('.')
-    vcf = VCF(str(vcf_p))
-
-    # Define a class to store the annotation record
-    csq_header = next(header for header in vcf.header_iter() if header['ID'] == 'CSQ')
-    csq_format = re.search(r'Format: ([\w\|]+)[\'"]', csq_header['Description']).group(1)
-    CSQ = namedtupled('CSQ', csq_format.split('|'))
-
-    for mut in vcf:
-        # Convert chromosome
-        if mut.CHROM == 'chrM':
-            chrom = 'MT'
-        else:
-            chrom = mut.CHROM[3:]
-
-        # Obtain read counts
-        read_counts = list(map(int, cal_read_counts(mut, caller)))
-
-        # Convert all the reported effects into CSQ objects
-        all_effects = [
-            CSQ(*eff_str.replace('&', ',').split('|'))
-            for eff_str in mut.INFO['CSQ'].split(',')
-        ]
-
-        # Pick one effect to describe the mutation
-        eff = pick_effect(mut, all_effects)
-
-        yield RawMutationCall(
-            sample,
-            chrom, mut.start, mut.end,
-            mut.REF, mut.ALT[0],
-            mut.FILTER,
-            caller,
-            *read_counts,
-            *eff
-        )
-
-
 if __name__ == '__main__':
     setup_cli()
-    main(snakemake.output['db'], snakemake.input['raw_vcfs'])
+    main(snakemake.output['db'], snakemake.input['raw_vcfs'], snakemake.input['annotated_mafs'])
