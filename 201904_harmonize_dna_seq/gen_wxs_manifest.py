1d3995a83f5169a6d6877249b8b933c5914eb999
Liang-Bo Wang <liang-bo.wang@wustl.edu>
2019-07-10 12:56:41 -0500

    Generate new custom WXS/WGS BAM manifest

--- 201904_harmonize_dna_seq/gen_wxs_manifest.py
+++ 201904_harmonize_dna_seq/gen_wxs_manifest.py
@@ -3,11 +3,13 @@ from pathlib import Path
 import uuid
 
 # Path to the unharmonized cases
-CASE_LIST_PTH = '../201904_locate_adhoc_data/tracked_results/unharmonized_cases.list'
-# Path to the sequencing file mapping from Matt's catalog
-MGI_MAP_PTH = '../matt_catalog/MGI.BamMap.dat'
+CASE_LIST_PTH = '../201907_locate_data/tracked_results/wxs_or_wgs_unharmonized_cases.list'
+
+CUSTOM_BAM_ROOT = 'processed_data/wxs_mark_dup_bam'
+
 # Path to the output manifest file
-OUT_MANIFEST_PTH = '../201904_locate_adhoc_data/tracked_results/MGI.GBM_custom_wxs.BamMap.dat'
+PREV_OUT_MANIFEST_PTH = '../201904_locate_adhoc_data/tracked_results/MGI.GBM_custom_wxs.BamMap.dat'
+OUT_MANIFEST_PTH = '../201907_locate_data/tracked_results/MGI.GBM_custom_wxs.BamMap.dat'
 
 # Read the list of all the unharmonized cases
 unharmonized_cases = set(open(CASE_LIST_PTH).read().splitlines())
@@ -15,44 +17,29 @@ unharmonized_cases = set(open(CASE_LIST_PTH).read().splitlines())
 # Build the sample (case, sample_type) BAM map on MGI
 # Data structure of the map
 # (case, sample_type) -> bam_pth
+all_wxs_custom_bams = sorted(Path(CUSTOM_BAM_ROOT).resolve().glob('*.bam'))
 wxs_bam_map = {}
-wgs_bam_map = {}
-with open(MGI_MAP_PTH) as f:
+for pth in all_wxs_custom_bams:
+    case, sample_type = pth.stem.split('_', 1)
+    wxs_bam_map[(case, sample_type)] = pth
+
+# Read the previous manifest to maintain the UUID
+previous_manifest_uuids = {}
+with open(PREV_OUT_MANIFEST_PTH) as f:
     reader = csv.DictReader(f, dialect='excel-tab')
     for row in reader:
-        case = row['case']
-
-        # Keep only the unharmonized cases of interest
-        if not (case in unharmonized_cases \
-                and row['reference'] == 'hg19' \
-                and row['data_format'] == 'BAM'):
-            continue
-
-        # Get the corresponding bam map
-        if row['experimental_strategy'] == 'WGS':
-            bam_map = wgs_bam_map
-        elif row['experimental_strategy'] == 'WXS':
-            bam_map = wxs_bam_map
-
-        sample = (case, row['sample_type'])
-        bam_pth = Path(row['data_path'])
-        bam_map[sample] = bam_pth
-
-# At the moment we only test for the first 20 cases
-all_wxs_samples = [f'{case}_{st}' for (case, st) in sorted(wxs_bam_map.keys())]
+        previous_manifest_uuids[row['# sample_name']] = row['UUID']
 
 
-current_abs_pth = Path().resolve()
-
+# Generate the manifest
 columns = [
     '# sample_name', 'case', 'disease', 'experimental_strategy', 'sample_type',
     'data_path', 'filesize', 'data_format', 'reference', 'UUID', 'system'
 ]
 with open(OUT_MANIFEST_PTH, 'w') as f:
-    writer = csv.writer(f, dialect='excel-tab')
+    writer = csv.writer(f, dialect='excel-tab', lineterminator='\n')
     writer.writerow(columns)  # Write header
-    for sample in all_wxs_samples:
-        case, sample_type = sample.split('_', 1)
+    for (case, sample_type), data_pth in wxs_bam_map.items():
         if sample_type == 'blood_normal':
             abbrv = 'N'
         elif sample_type == 'tumor':
@@ -60,15 +47,15 @@ with open(OUT_MANIFEST_PTH, 'w') as f:
         else:
             raise ValueError(f'Unknown sample type {sample_type}')
         sample_name = f'{case}.WXS.{abbrv}.hg38-bobo'
-        data_pth = Path(current_abs_pth,
-                        'processed_data/wxs_mark_dup_bam',
-                        f'{sample}.bam')
-        if not data_pth.exists():
-            print(f'{sample} does not have BAM!')
-            file_size = 0
-        else:
-            file_size = data_pth.stat().st_size
-        data_id = str(uuid.uuid4())
+        file_size = data_pth.stat().st_size
+
+        # Reuse the UUID
+        try:
+            data_id = previous_manifest_uuids[sample_name]
+        except KeyError:
+            # Generate a new UUID
+            data_id = str(uuid.uuid4())
+
         writer.writerow([
             sample_name, case, 'GBM', 'WXS', sample_type,
             str(data_pth), str(file_size), 'BAM', 'hg38', data_id, 'MGI',
